9047b5a9884408e4123367876aeadb5ee9d17008
Igor K. <ikadochnikov@roox.ru>
2026-03-05 18:49:39 +0500

    - исправлен mongo-init.js

--- init/mongo-init.js
+++ init/mongo-init.js
@@ -1,7 +1,10 @@
-db = db.getSiblingDB("app");
+db = db.getSiblingDB("checker");
 
 db.createCollection("results");
+db.createCollection("tasks");
+db.createCollection("prompts");
+db.createCollection("submissions");
 
-db.results.createIndex({ task_id: 1 });
-db.results.createIndex({ created_at: -1 });
-db.results.createIndex({ candidate_id: 1 });
+db.results.createIndex({task_id: 1});
+db.results.createIndex({created_at: -1});
+db.results.createIndex({candidate_id: 1});
